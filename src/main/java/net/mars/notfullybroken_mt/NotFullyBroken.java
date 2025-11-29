67cdeb8df213dc48f981eb3278613910879043c2
MDen-MT <danilalikan@gmail.com>
2025-11-29 13:02:35 +0500

    Initial commit

---
+++ src/main/java/net/mars/notfullybroken_mt/NotFullyBroken.java
@@ -0,0 +1,23 @@
+package net.mars.notfullybroken_mt;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NotFullyBroken implements ModInitializer {
+	public static final String MOD_ID = "notfullybroken_mt";
+	public static final double MDECILLION = Math.pow(10, 256);
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModContainer mod = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(null);
+		String version = mod.getMetadata().getVersion().getFriendlyString();
+
+		LOGGER.info("[{}] v{} initialized by MARS Team.", MOD_ID, version);
+	}
+}
