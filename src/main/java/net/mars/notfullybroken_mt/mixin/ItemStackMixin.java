f482c5f77ead298d29e5f861c61592809d7c8b55
MDen-MT <danilalikan@gmail.com>
2025-11-30 14:06:17 +0500

    feat: handle breaking of a tool that has a broken state
    On break, it plays a break sound, creates particles, and adds it to statistics.

--- src/main/java/net/mars/notfullybroken_mt/mixin/ItemStackMixin.java
+++ src/main/java/net/mars/notfullybroken_mt/mixin/ItemStackMixin.java
@@ -1,30 +1,43 @@
 package net.mars.notfullybroken_mt.mixin;
 
-import net.mars.notfullybroken_mt.util.ModTags;
 import net.mars.notfullybroken_mt.util.NotFullyBrokenHelper;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 	@Shadow public abstract ItemStack copy();
-
-	@Shadow public abstract boolean isIn(TagKey<Item> tag);
+	@Shadow public abstract int getDamage();
+	@Shadow public abstract Item getItem();
+	@Shadow public abstract int getMaxDamage();
 
 	@Inject(method = "shouldBreak", at = @At("HEAD"), cancellable = true)
 	private void disableBreaking(CallbackInfoReturnable<Boolean> cir) {
 		if (NotFullyBrokenHelper.isInBrokenState(this.copy())) {
 			cir.setReturnValue(false);
+		}
+	}
+
+	@Inject(method = "onDurabilityChange", at = @At("HEAD"))
+	private void ItemBreakHandler(int damage, @Nullable ServerPlayerEntity player, Consumer<Item> breakCallback, CallbackInfo ci) {
+		Item item = this.getItem();
+		ItemStack damagedStack = this.copy();
+		damage = MathHelper.clamp(damage, 0, this.getMaxDamage());
+		damagedStack.setDamage(damage);
+		if (NotFullyBrokenHelper.isInBrokenState(damagedStack) && this.getDamage()<damage) {
+			breakCallback.accept(item);
 		}
 	}
 }
